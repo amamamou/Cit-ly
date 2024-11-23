901cf7a0ab4212ca5f52d36d9373a538d9efb93e
amamamou <amamoumaram88@gmail.com>
2024-11-23 04:38:17 +0100

    Authors

--- citely-frontend/citely-frontend/src/app/app-routing.module.ts
+++ citely-frontend/citely-frontend/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { AuthorsComponent } from './authors/authors.component';
 
 
 const routes: Routes = [
   { path: '', component: HomeComponent }, // Default route
+  { path: 'authors', component: AuthorsComponent }, // Add Authors route
+
+
 ];
 
 @NgModule({
